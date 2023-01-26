79b689bd1e12ca81c5bcb5022c322bd1f5b10a85
JanajanJ <janajanjeyabalan@gmail.com>
2023-01-26 18:12:05 -0500

    Update main_script.py
    Add the team names functions

--- main_script.py
+++ main_script.py
@@ -8,3 +8,23 @@ def get_number_of_teams():
         print("The minimum number of teams is 2, try again")
 
     return num_teams
+
+
+    def get_team_names(num_teams):
+        team_names = []
+
+    for idx in range(num_teams):
+        while True:
+            team_name = input(f"Enter the name for team #{idx + 1}: ")
+            num_words = len(team_name.split(" "))
+
+            if num_words > 2:
+                print("Team names may have at most 2 words, try again.")
+            elif len(team_name) < 2:
+                print("Team names must have at least 2 character, try again.")
+            else:
+                break
+
+        team_names.append(team_name)
+
+    return team_names
