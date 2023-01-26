484e7ab28d59ed069be4d997896a882c97b43dc6
JanajanJ <janajanjeyabalan@gmail.com>
2023-01-26 18:15:49 -0500

    Update main_script.py
    Adding the function for the second item, and added the for loops

--- main_script.py
+++ main_script.py
@@ -60,4 +60,29 @@ def get_team_wins(team_names, games_played):
 
         team_wins.append((team, wins))
 
-    return team_wins        
+    return team_wins
+
+
+def get_second_item(item):
+    return item[1]
+
+
+num_teams = get_number_of_teams()
+team_names = get_team_names(num_teams)
+games_played = get_number_of_games_played(num_teams)
+team_wins = get_team_wins(team_names, games_played)
+
+print("Generating the games to be played in the first round of the tournament...")
+sorted_teams = sorted(team_wins, key=get_second_item)
+game_pairings = []
+
+games_to_make = len(sorted_teams)//2
+
+for game_num in range(games_to_make):
+    home_team = sorted_teams[game_num][0]
+    away_team = sorted_teams[num_teams - 1 - game_num][0]
+    game_pairings.append([home_team, away_team])
+
+for pairing in game_pairings:
+    home_team, away_team = pairing
+    print(f"Home: {home_team} VS Away: {away_team}")           
